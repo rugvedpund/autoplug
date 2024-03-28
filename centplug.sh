9956da5d9ecc0c79be04bebfda19af7ebe1f4f20
Rugved Pund <rugvedpund@gmail.com>
2024-03-28 19:23:56 -0400

    change to printf to have cleaner stdout

--- centplug.sh
+++ centplug.sh
@@ -6,7 +6,7 @@ while true; do
     gpuutil=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
     plugstatus=$(smartthings devices:status ff91e22e-219c-4ba2-924f-1f7c8c9ecef4 -j | jq -r ".components.main.switch.switch.value")
     timestamp=$(date +%r)
-    echo "$timestamp | GPU Util: $gpuutil, Plug Status: $plugstatus, GPU Temp: $gputemp"  
+    printf "$timestamp | GPU Util: $gpuutil, Plug Status: $plugstatus, GPU Temp: $gputemp \r"  
     sleep 5
     # if [ $gputemp -gt 40 ] && [[ $plugstatus == "off" ]]; then
     if [ $gpuutil -gt 10 ] && [[ $plugstatus == "off" ]]; then
