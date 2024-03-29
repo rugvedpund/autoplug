042341f4add6a34d5b57fd7d4b4e7daa2de40900
Rugved Pund <rugvedpund@gmail.com>
2024-03-28 22:32:08 -0400

    minor fixes

--- centplug.sh
+++ centplug.sh
@@ -6,15 +6,15 @@ while true; do
     gpuutil=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
     plugstatus=$(smartthings devices:status ff91e22e-219c-4ba2-924f-1f7c8c9ecef4 -j | jq -r ".components.main.switch.switch.value")
     timestamp=$(date +%r)
-    printf "$timestamp | GPU Util: $gpuutil, Plug Status: $plugstatus, GPU Temp: $gputemp \r"  
-    sleep 5
+    printf "$timestamp | GPU Util: $gpuutil %, Plug Status: $plugstatus, GPU Temp: $gputemp \r"  
+    sleep 10
     # if [ $gputemp -gt 40 ] && [[ $plugstatus == "off" ]]; then
     if [ $gpuutil -gt 10 ] && [[ $plugstatus == "off" ]]; then
-        echo "$timestamp | Turning on plug..."
+        printf "\n$timestamp | Turning on plug...\n"
         smartthings devices:commands ff91e22e-219c-4ba2-924f-1f7c8c9ecef4 switch:on
     # elif [ $gputemp -lt 40 ] && [[ $plugstatus == "on" ]]; then
     elif [ $gpuutil -lt 10 ] && [[ $plugstatus == "on" ]]; then
-        echo "$timestamp | Turning off plug..."
+        printf "\n$timestamp | Turning off plug...\n"
         smartthings devices:commands ff91e22e-219c-4ba2-924f-1f7c8c9ecef4 switch:off
     fi
 
